b0c9c67d8e60859e0ce8f8461022d6a60d3f84bc
Quasar Wei <quasar.wei@outlook.com>
2019-11-20 14:24:44 -0800

    deploy and style

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import LangService from '../../services/lang-service';
 import LangContext from '../../contexts/LangContext';
+import { Label, Input } from '../../components/Form/Form';
+import Button from '../../components/Button/Button';
 import './LearningRoute.scss';
 
 class LearningRoute extends Component {
   static contextType = LangContext;
+
+/*   constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+  } */
 
   state = {
     renderForm: true
@@ -14,7 +21,7 @@ class LearningRoute extends Component {
     this.handleNextWord();
   }
 
-  handleSubmitGuess = e => {
+  handleSubmitGuess = (e) => {
     e.preventDefault();
     LangService.postGuess(this.context.guess).then(res => {
       this.context.setResponse(res);
@@ -22,10 +29,16 @@ class LearningRoute extends Component {
     });
   };
 
+/*   handleAlternateSubmitGuess = (e) => {
+    this.context.setGuess(e.target.value);
+    this.handleSubmitGuess();
+  } */
+
   handleNextWord = () => {
     LangService.getHead().then(res => {
       this.context.setHead(res);
       this.setState({ renderForm: true });
+      this.context.setGuess('');
     });
   };
 
@@ -33,33 +46,36 @@ class LearningRoute extends Component {
     let head = this.context.head || {};
     return (
       <>
-        <h2>Translate the word:</h2>
+        <div className='scoreboard'>
+          <div className='correct-count'>
+            <h4>{head.wordCorrectCount}</h4>
+          </div>
+          <div className='totalscore'>
+            <h3>{head.totalScore}</h3>
+          </div>
+          <div className='incorrect-count'>
+            <h4>{head.wordIncorrectCount}</h4>
+          </div>
+        </div>
         <span className='word'>{head.nextWord}</span>
         <form className='guess-form' onSubmit={e => this.handleSubmitGuess(e)}>
           <fieldset>
-            <label htmlFor='learn-guess-input'>
+            <Label htmlFor='learn-guess-input'>
               What's the translation for this word?
-            </label>
-            <input
+            </Label>
+            <Input
               type='text'
               id='learn-guess-input'
               name='guess'
               maxLength='1'
+              autoFocus
               value={this.context.guess || ''}
               onChange={e => this.context.setGuess(e.target.value)}
               required
-            ></input>
-            <button type='submit'>Submit your answer</button>
+            />
+            <Button type='submit'>Submit your answer</Button>
           </fieldset>
         </form>
-        <p>Your total score is: {head.totalScore}</p>
-        <p>
-          You have answered this word correctly {head.wordCorrectCount} times.
-        </p>
-        <p>
-          You have answered this word incorrectly {head.wordIncorrectCount}{' '}
-          times.
-        </p>
       </>
     );
   };
@@ -74,31 +90,32 @@ class LearningRoute extends Component {
             ? 'You were correct! :D'
             : 'Good try, but not quite right :('}
         </h2>
-        <span className='word'>{response.nextWord}</span>
         <div className='DisplayFeedback'>
+        <span className='word'>{head.nextWord}</span>
           <p>
             The correct translation for {head.nextWord} was {response.answer}{' '}
             and you chose {this.context.guess} !
           </p>
         </div>
-        <button onClick={this.handleNextWord}>Try another word!</button>
+        <Button autoFocus onClick={this.handleNextWord}>Try another word!</Button>
         <div className='DisplayScore'>
           <p>Your total score is: {response.totalScore}</p>
         </div>
-        <p>
+    {/*     <p>
           You have answered this word correctly {response.wordCorrectCount}{' '}
           times.
         </p>
         <p>
           You have answered this word incorrectly {response.wordIncorrectCount}{' '}
           times.
-        </p>
+        </p> */}
       </>
     );
   };
 
   render() {
     let renderForm = this.state.renderForm;
+    
     return (
       <section className='learningRoute-section'>
         {renderForm ? this.renderForm() : this.renderResponse()}
