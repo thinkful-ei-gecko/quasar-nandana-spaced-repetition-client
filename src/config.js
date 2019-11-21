30b4872c581c88895ae6ffdff8463237c350a08f
Quasar Wei <quasar.wei@outlook.com>
2019-11-21 12:00:01 -0800

    styling

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 export default {
   API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  TOKEN_KEY: 'blogful-client-auth-token'
 }
 
-// process.env.REACT_APP_TOKEN_KEY
+// 'blogful-client-auth-token'
+// process.env.REACT_APP_TOKEN_KEY,
 // http://localhost:8000/api
 // https://whispering-hollows-62849.herokuapp.com/api
